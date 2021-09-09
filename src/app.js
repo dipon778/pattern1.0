ead97dbe00844be5c79c21bfa97e23f17e4aa6ac
lool <lool@localhost.localdomain>
2021-09-09 16:20:27 +0600

    clean code

--- src/app.js
+++ src/app.js
@@ -1,95 +1,40 @@
+
+//Declaring storing variable
 var datas,num=-1;
+
+//function for generating data 
 function equation() {
 
-    // var c = document.getElementById("myCanvas");
-    // var ctx = c.getContext("2d");
     datas = [];
+    for (var i = 0; i < 100; i++) {
 
-    for (var i = 1; i < 100; i++) {
+        //var sum = document.getElementById('user-input').value; 
+        //console.log(sum);
 
+        //The equation 
         var sum = 3 * i + 1;
+
+        // if even divide by 2
         if (sum % 2 === 0) {
-            firstpoint = sum;
+            
             sum = sum / 2;
             datas[i]=sum;
-            //document.getElementById('myList').innerHTML += `<li> ${sum} </li>`
-            // ctx.moveTo(10, 100);
-            // ctx.lineTo(200,800);
-            // ctx.stroke();
             
-
-        } else if (sum % 2 != 0) {
-            //document.getElementById('myList').innerHTML += `<li> ${sum} </li>`
-            datas[i]=sum;
-            // ctx.moveTo(0, 0);
-            // ctx.lineTo(0, 00);
-            // ctx.stroke();
-            
-        }
-        
+        //if odd store as it is 
+        } else if (sum % 2 != 0) {    
+            datas[i]=sum;    
+        }        
     }
-
-
-    // let n = 0;
-
-    // sum = 3*n+1;
-
-    // if(sum %2 === 0){
-    //     n = sum /2 ;
-    // } else if(sum % 2  != 0){
-    //     n = sum;
-    // }
-    
-    // return sum;
-    console.log(datas);
-    
-
 }
 equation();
+
 function getData(){
     if( num < datas.length){
         num ++;
     return datas[num];
-    }
-    
-    
+    }    
 }
 
-
-// window.onload = function () {
-//     var datar = equation();
-//     var chart = new CanvasJS.Chart("chartContainer",
-//     {
-
-//       title:{
-//       text: "Earthquakes - per month"
-//       },
-//        data: [
-
-//       {
-//         type: "line",
-
-//         dataPoints: [
-//             {x:datar[1],y:10},
-//             {x:datar[2],y:20},
-//             {x:datar[3],y:30},
-//             {x:datar[4],y:40},
-//             {x:datar[5],y:50},
-//             {x:datar[6],y:60},
-//             {x:datar[7],y:70},
-//         ]
-//       }
-//       ]
-//     });
-
-//     chart.render();
-//   }
-
-
-
-// function getData() {
-//     return Math.random();
-//  }  
 Plotly.newPlot('chart',[{
     y:[getData()],
     type:'line'
@@ -106,4 +51,4 @@ setInterval(function(){
             }
         });
     }
-},200);
+},400);
