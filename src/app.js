2d01c62b78aa48a8fa9c822c66b49476ba084b68
smalmahin <mahin.ikaras123@gmail.com>
2021-11-09 22:49:38 +0600

    change in line 10

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var sum,datas,num=-1,iterations;;
 function equation(sum) {
     for (var i = 0; i < 1000; i++) {         
         // if even divide by 2
-        if (sum % 2 == 0) {     ,AS qknwkqwekwkqe kqwjhekjgjwq kekwjqhe  maksa kasbjkjkhn 
+        if (sum % 2 == 0) {     hgfdgfdsgfdgfdgfd 
             sum = sum / 2;
             datas[i]=sum;
             if(sum == 2){
